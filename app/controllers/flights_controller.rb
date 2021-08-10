51fb2c15681bf59d791b8aaaec64f186c9cc5023
berubenic <berubenic@gmail.com>
2021-08-10 16:07:34 -0400

    Form submit returns list of valid flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,10 @@
 class FlightsController < ApplicationController
     def index
-        
+        if params[:flight_search].present?
+            start_airport = params[:flight_search][:start_airport_id]
+            end_airport = params[:flight_search][:end_airport_id]            
+            @flights = Flight.where("start_airport_id = :start_airport_id and end_airport_id = :end_airport_id", 
+                    { start_airport_id: start_airport, end_airport_id: end_airport })
+        end
     end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,5 +3,4 @@ class Flight < ApplicationRecord
     belongs_to :end_airport, :class_name => 'Airport'
 
     validates_presence_of :start_airport_id, :end_airport_id, :start_datetime, :flight_duration_minutes 
-    validate :
 end
