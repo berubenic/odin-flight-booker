a3304e48b30412b1e9fc28988c5da8462dab33b9
berubenic <berubenic@gmail.com>
2021-08-11 15:24:01 -0400

    Booking and Passenger model with migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_211423) do
+ActiveRecord::Schema.define(version: 2021_08_11_192315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,11 +22,25 @@ ActiveRecord::Schema.define(version: 2021_08_09_211423) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.integer "passenger_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "flights", force: :cascade do |t|
     t.integer "start_airport_id", null: false
     t.integer "end_airport_id", null: false
     t.datetime "start_datetime", null: false
     t.integer "flight_duration_minutes", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
