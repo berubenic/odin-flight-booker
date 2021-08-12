51f150f68fa16b3a83b67ef603d3d77cab1308e1
berubenic <berubenic@gmail.com>
2021-08-12 19:51:31 -0400

    FlightsController request specs

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,44 +1,44 @@
 class FlightsController < ApplicationController
     def index
-        @flight_search = FlightSearch.new
-       
-        if params[:flight_search].present?
-            date = convert_dates_to_datetime(params[:flight_search][:date]) if params[:flight_search][:date].present?          
-            @flight_search = flight_search_params(@flight_search, params[:flight_search], date)
-            @number_of_passengers = params[:flight_search][:number_of_passengers]
-
-            
+        if params[:flight_search]
+            @flight_search ||= FlightSearch.new(flight_search_params)
+            @flight_search.date = convert_dates_to_datetime(params[:flight_search][:date])
+            @flight_search.number_of_passengers = params[:flight_search][:number_of_passengers]
             respond_to do |format|
                 if @flight_search.valid?
-                    start_airport = params[:flight_search][:start_airport_id]
-                    end_airport = params[:flight_search][:end_airport_id]
                     @flights = Flight.where("start_airport_id = :start_airport_id and
                                          end_airport_id = :end_airport_id and start_datetime >= :date",
-                                        { start_airport_id: start_airport, 
-                                            end_airport_id: end_airport,
-                                            date: date })
+                                        { start_airport_id: params[:flight_search][:start_airport_id], 
+                                            end_airport_id: params[:flight_search][:end_airport_id],
+                                            date: @flight_search.date })
                     unless @flights.present?
                         flash.now[:notice] = "No matching flights available"
+                        format.html{render 'index' }
                     end
+                    flash.now[:notice] = "Flights found: "
                     format.html{render 'index' }
                 else
+                    flash.now[:notice] = "No matching flights available"
                     format.html{render 'index' }
                 end
-            end                
+            end 
+        else
+            @flight_search = FlightSearch.new
+            render 'index'
         end
     end
 
     private
 
-    def flight_search_params(flight_search, params, date)
-        flight_search.date = date
-        flight_search.number_of_passengers = params[:number_of_passengers]
-        flight_search.start_airport_id = params[:start_airport_id]
-        flight_search.end_airport_id = params[:end_airport_id]
-        flight_search
+    def flight_search_params
+        params.require(:flight_search).permit(:number_of_passengers, :date, :start_airport_id, :end_airport_id)
     end
 
     def convert_dates_to_datetime(date)
-        Date.parse(date)
+       if date.present?
+            Date.parse(date)
+       else
+            Date.today
+       end
     end
 end
---
+++ spec/requests/flights_controller_spec.rb
@@ -0,0 +1,20 @@
+require "rails_helper"
+
+RSpec.describe "FlightsController", :type => :request do
+  
+  describe "#index" do
+    
+    it "displays notice when no flights match search" do
+      get "/", params: { :flight_search => {:date => Date.today, :number_of_passengers => 0, :start_airport_id => nil, :end_airport_id => nil }}
+      expect(response).to have_http_status(:ok)
+      expect(flash[:notice]).to eq('No matching flights available')
+    end
+
+    it "displays notice when flights are found" do
+      get "/", params: { :flight_search => {:date => Date.today, :number_of_passengers => 1, :start_airport_id => 1, :end_airport_id => 1 }}
+      expect(response).to have_http_status(:ok)
+      expect(flash[:notice]).to eq('Flights found: ')
+    end
+  end
+
+end
